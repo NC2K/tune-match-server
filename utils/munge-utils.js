1bef1c5c82cc49a006983c7c638e94a2fc49ec31
Katherine Tam <katherinemtam@gmail.com>
2021-05-25 09:40:38 -0700

    adjust and test munge utils

--- utils/munge-utils.js
+++ utils/munge-utils.js
@@ -1,6 +1,9 @@
 export function mungeSearch(arr) {
-    
-  return arr.map(data => {
+  
+  // const newArr = arr.replace(/(\r\n|\n|\r)/gm, '');
+  // const key = Object.keys(newArr)[1];
+  // console.log('NEW ARRAY:', arr);
+  return arr.results.map(data => {
     return {
       title: data.trackName,
       artist: data.artistName,
